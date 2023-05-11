685bcf8ee2a948a8e9c17a487ff80e1bd1040822
KingFarGrace <kingfargrace@163.com>
2023-05-12 00:10:20 +0100

    Skeleton of user functions - router_handler: handler functions to be called when a certain router has been   visited.

---
+++ backend/router/user.js
@@ -0,0 +1,9 @@
+const router = require('router')
+
+router.post("/login", )
+
+router.post("/register", )
+
+router.post("/user/update", )
+
+router.post("/user/update/pwd", )
---
+++ backend/router_handler/user_hadler.js
@@ -0,0 +1,15 @@
+function login(res, req) {
+
+}
+
+function register(res, req) {
+
+}
+
+function update(res, req) {
+
+}
+
+function updatePwd(res, req) {
+
+}
