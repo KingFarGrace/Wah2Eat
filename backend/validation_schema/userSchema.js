c33f73da586c1864d0f2bf061e92c1e7487e6ce7
KingFarGrace <kingfargrace@163.com>
2023-05-16 09:21:53 +0100

    Debug: - User password regular expression.

--- backend/validation_schema/userSchema.js
+++ backend/validation_schema/userSchema.js
@@ -3,13 +3,13 @@ const joi = require('joi')
 module.exports.registerSchema = joi.object({
     email: joi.string().email().required(),
     username: joi.string().alphanum().min(1).max(16).required(),
-    password: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,16}')).min(6).max(16).required(),
+    password: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{6,16}')).min(6).max(16).required(),
     repeatPwd: joi.ref('password')
 })
 
 module.exports.loginSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,16}')).min(6).max(16).required()
+    password: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{6,16}')).min(6).max(16).required()
 })
 
 module.exports.updateSchema = joi.object({
@@ -22,7 +22,7 @@ module.exports.updateSchema = joi.object({
 
 module.exports.updatePwdSchema = joi.object({
     email: joi.string().email().required(),
-    oldPwd: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,16}')).min(6).max(16).required(),
-    newPwd: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,16}')).min(6).max(16).required(),
+    oldPwd: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{6,16}')).min(6).max(16).required(),
+    newPwd: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{6,16}')).min(6).max(16).required(),
     repeatNewPwd: joi.ref('newPwd')
 })
